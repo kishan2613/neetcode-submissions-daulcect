99e6e086676aca903514800f94327aa64953e28f
kishan2613 <160527834+kishan2613@users.noreply.github.com>
2026-09-08 01:18:16 +0530

    Add: permutations - submission-0

---
+++ Data Structures & Algorithms/permutations/submission-0.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> permute(int[] nums) {
+        List<List<Integer>> res = new ArrayList<>();
+        dfs(nums,new ArrayList<>(),res);
+        return res;
+    }
+
+    public void dfs(int nums[], List<Integer> path, List<List<Integer>> res){
+        
+        if(path.size()==nums.length){
+        res.add(new ArrayList<>(path));
+        return;
+        }
+
+        for(int i=0;i<nums.length;i++){
+            if(path.contains(nums[i])){
+                continue;
+            }
+
+            path.add(nums[i]);
+
+            dfs(nums,path,res);
+
+            path.remove(path.size()-1);
+        }
+    }
+}
